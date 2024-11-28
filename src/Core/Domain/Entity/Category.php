da12299121e77c27eedc4fa85598ba78e8e3daeb
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-11-28 20:52:18 -0300

    feat: work on tests and class category

--- src/Core/Domain/Entity/Category.php
+++ src/Core/Domain/Entity/Category.php
@@ -2,15 +2,57 @@
 
 namespace Core\Domain\Entity;
 use Core\Domain\Entity\Traits\MethodsMagicsTrait;
+use Core\Domain\Exception\EntityValidationException;
+
 
 class Category
 {
     use MethodsMagicsTrait;
     public function __construct(
         protected string $id = '',
-        protected string $name,
+        protected string $name = '',
         protected string $description = '',
         protected bool $isActive = true
-    ) {}
+    ) {
+
+        $this->validate();
+    }
+
+    public function activate(): void
+    {
+        $this->isActive = true;
+    }
+
+    public function disable(): void
+    {
+        $this->isActive = false;
+    }
+    public function update(string $name, string $description, bool $isActive): void
+    {
+        $this->name = $name;
+        $this->description = $description;
+        $this->isActive = $isActive;
+
+        $this->validate();
+    }
+
+    public function validate()
+    {
+        if (empty($this->name)) {
+            throw new EntityValidationException('Name is required');
+        }
+
+        if (strlen($this->name) < 3 || strlen($this->name) > 100) {
+            throw new EntityValidationException('Name must be between 3 and 100 characters');
+        }
+
+        if (empty($this->description)) {
+            throw new EntityValidationException('Description is required');
+        }
+
+        if (strlen($this->description) < 3 || strlen($this->description) > 100) {
+            throw new EntityValidationException('Description must be between 3 and 100 characters');
+        }
+    }
 
 }
--- tests/Unit/Domain/Entity/CategoryUnitTest.php
+++ tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Unit\Domain\Entity;
 
-
 use Core\Domain\Entity\Category;
+use Core\Domain\Exception\EntityValidationException;
 use PHPUnit\Framework\TestCase;
 
 class CategoryUnitTest extends TestCase
@@ -11,7 +11,6 @@ class CategoryUnitTest extends TestCase
     public function testAttributes()
     {
         $category = new Category(
-            id: '',
             name: 'Category Name',
             description: 'Category Description',
             isActive: true
@@ -20,7 +19,52 @@ class CategoryUnitTest extends TestCase
         $this->assertEquals('Category Name', $category->getName());
         $this->assertEquals('Category Description', $category->getDescription());
         $this->assertEquals(true, $category->isActive());
-
     }
 
+    public function testActivated()
+    {
+        $category = new Category(
+            name: 'Category Name',
+            description: 'Category Description',
+            isActive: true
+        );
+
+        $this->assertTrue($category->isActive());
+        $category->disable();
+        $this->assertFalse($category->isActive());
+    }
+
+    public function testUpdate()
+    {
+        $uuid = 'uuid.value';
+        $category = new Category(
+            id: $uuid,
+            name: 'Category Name',
+            description: 'Category Description',
+            isActive: true
+        );
+
+        $category->update(
+            name: 'New Category Name',
+            description: 'New Category Description',
+            isActive: false
+        );
+
+        $this->assertEquals('New Category Name', $category->getName());
+        $this->assertEquals('New Category Description', $category->getDescription());
+        $this->assertEquals(false, $category->isActive());
+    }
+
+    public function testValidate()
+    {
+        $this->expectException(EntityValidationException::class);
+        $this->expectExceptionMessage('Name is required');
+
+        $category = new Category(
+            name: '',
+            description: 'Category Description'
+        );
+
+        $category->validate();
+    }
 }
